086506eea4c90db971868a6ab380f98638a4426a
Navid M <navidmomtahen@gmail.com>
2025-08-07 19:42:31 +0100

    further numeric amendments

--- preprocessor/preprocs.go
+++ preprocessor/preprocs.go
@@ -45,9 +45,11 @@ func InsertMacros(output string) string {
 	}
 	if strings.Contains(output, "i32") || strings.Contains(output, "u32") || strings.Contains(output, "i64") ||
 		strings.Contains(output, "u64") || strings.Contains(output, "i16") || strings.Contains(output, "u16") ||
-		strings.Contains(output, "u8") || strings.Contains(output, "i8") {
+		strings.Contains(output, "u8") || strings.Contains(output, "i8") || strings.Contains(output, "f64") ||
+		strings.Contains(output, "f32") {
 		outp = "#include <stdint.h>\ntypedef int32_t i32;\ntypedef uint32_t u32;\ntypedef int64_t i64;\n" +
-			"typedef uint64_t u64;\ntypedef int16_t i16;\ntypedef uint16_t u16;\ntypedef uint8_t u8;\ntypedef int8_t i8;\n" + outp
+			"typedef uint64_t u64;\ntypedef int16_t i16;\ntypedef uint16_t u16;\ntypedef uint8_t u8;\ntypedef int8_t i8;\n" +
+			"typedef double f64;\ntypedef float f32;\n" + outp
 	}
 	return outp
 }
