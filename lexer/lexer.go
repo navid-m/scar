a79209312711e177807c002a021a93850496a2e7
Navid M <navidmomtahen@gmail.com>
2025-08-11 17:09:54 +0100

    custom types in top level function parameters

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -610,6 +610,14 @@ func isValidType(s string) bool {
 		mapTypeContent := strings.TrimPrefix(strings.TrimSuffix(s, "]"), "map[")
 		return isValidMapType(mapTypeContent)
 	}
+	if len(s) > 0 && s[0] >= 'A' && s[0] <= 'Z' {
+		for _, r := range s {
+			if !((r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') || r == '_') {
+				return false
+			}
+		}
+		return true
+	}
 	return false
 }
 
