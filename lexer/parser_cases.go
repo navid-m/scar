3623370d573a0a1a7a88a4f69c8d1decf636de4d
Navid M <navidmomtahen@gmail.com>
2025-08-15 07:27:09 +0100

    fmt

--- lexer/parser_cases.go
+++ lexer/parser_cases.go
@@ -857,7 +857,6 @@ func LoadModule(moduleName string, baseDir string) (*ModuleInfo, error) {
 		return nil, fmt.Errorf("failed to parse module '%s': %v", moduleName, err)
 	}
 
-	// Reorder function declarations to handle hoisting
 	hoistedStatements, err := HoistFunctions(program.Statements)
 	if err != nil {
 		return nil, fmt.Errorf("failed to hoist functions in module '%s': %v", moduleName, err)
