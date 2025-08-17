e4487248733403759f297d21c0ec8a579973af7c
Navid M <navidmomtahen@gmail.com>
2025-08-17 13:33:25 +0100

    amend formatting

--- meta/meta.go
+++ meta/meta.go
@@ -7,14 +7,27 @@
 package meta
 
 import (
-	"flag"
 	"fmt"
 )
 
 const Version = "v0.0.1-alpha.1"
 
 func ShowUsage() {
-	fmt.Println("Usage: scar [options] [program] | -v")
-	flag.PrintDefaults()
-	fmt.Printf("\nScar %v - By Navid M (c) 2025\n", Version)
+	fmt.Printf("Scar %v - By Navid M (c) 2025\n\n", Version)
+	fmt.Println("Usage:")
+	fmt.Println("  scar [options] [program]")
+	fmt.Println()
+	fmt.Println("Options:")
+
+	fmt.Printf("  %-10s %s\n", "-asm", "Show assembly output")
+	fmt.Printf("  %-10s %s\n", "-c", "Emit C output")
+	fmt.Printf("  %-10s %s\n", "-d", "Enable verbose logging")
+	fmt.Printf("  %-10s %s\n", "-dll", "Compile as dynamic link library")
+	fmt.Printf("  %-10s %s\n", "-gc", "Use bdwgc garbage collector")
+	fmt.Printf("  %-10s %s\n", "-keepc", "Keep generated C file")
+	fmt.Printf("  %-10s %s\n", "-l <opts>", "Additional linker options (e.g. -lm -lpthread)")
+	fmt.Printf("  %-10s %s\n", "-o <file>", "Output binary name")
+	fmt.Printf("  %-10s %s\n", "-opt", "Optimize for performance")
+	fmt.Printf("  %-10s %s\n", "-v", "Show version")
+	fmt.Printf("  %-10s %s\n\n", "-h, --help", "Show this help message")
 }
