ba9f14746bc1b8c6e3f3c28e07ac724c73fc9c12
Navid M <navidmomtahen@gmail.com>
2025-08-18 16:58:37 +0100

    handle edge cases

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -2842,7 +2842,12 @@ func renderStatements(b *strings.Builder, stmts []*lexer.Statement, indent strin
 						}
 					} else {
 						if !strings.HasPrefix(value, "\"") && !strings.HasSuffix(value, "\"") {
-							if _, isLocal := localVars[value]; isLocal {
+							if strings.Contains(value, "->") || strings.Contains(value, ".") {
+								if strings.Contains(value, ".") && !strings.Contains(value, "->") {
+									value = convertPropertyAccess(value)
+								}
+								fmt.Fprintf(b, "%sstrcpy(%s, %s);\n", indent, varName, value)
+							} else if _, isLocal := localVars[value]; isLocal {
 								fmt.Fprintf(b, "%sstrcpy(%s, %s);\n", indent, varName, value)
 							} else if _, isGlobal := globalVars[value]; isGlobal {
 								fmt.Fprintf(b, "%sstrcpy(%s, %s);\n", indent, varName, value)
