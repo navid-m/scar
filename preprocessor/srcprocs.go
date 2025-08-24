3f05b75f43ca0c94c7f848e1ef0aaa13b3587b00
Navid M <navidmomtahen@gmail.com>
2025-08-24 12:00:25 +0100

    use range over int

--- preprocessor/srcprocs.go
+++ preprocessor/srcprocs.go
@@ -136,7 +136,7 @@ func ProcessMacros(source string) string {
 
 	const maxIters = 10
 	prev := source
-	for i := 0; i < maxIters; i++ {
+	for range maxIters {
 		next := expandMacros(prev, macros)
 		if next == prev {
 			return next
